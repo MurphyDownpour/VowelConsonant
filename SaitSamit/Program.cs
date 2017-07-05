619ba0fdcf2d9d326771019740881fb0c3ead771
MurphyDownpour <alex_shepherd@bk.ru>
2017-07-05 13:12:00 +0400

    Vowel and consonant project

---
+++ SaitSamit/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaitSamit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Simvol daxil edin: ");
+            char a = Convert.ToChar(Console.ReadLine());
+                switch (a)
+                {
+                    case 'a':
+                        Console.WriteLine("Sait.");
+                        break;
+                    case 'A':
+                        Console.WriteLine("Sait.");
+                        break;
+                    case 'e':
+                        Console.WriteLine("Sait.");
+                        break;
+                    case 'E':
+                        Console.WriteLine("Sait.");
+                        break;
+                    case 'i':
+                        Console.WriteLine("Sait.");
+                        break;
+                    case 'I':
+                        Console.WriteLine("Sait.");
+                        break;
+                    case 'o':
+                        Console.WriteLine("Sait.");
+                        break;
+                    case 'O':
+                        Console.WriteLine("Sait.");
+                        break;
+                    case 'u':
+                        Console.WriteLine("Sait.");
+                        break;
+                    case 'U':
+                        Console.WriteLine("Sait.");
+                        break;
+                    default:
+                        Console.WriteLine("Samit");
+                        break;
+                }
+            }
+            
+        }
+    }
+
